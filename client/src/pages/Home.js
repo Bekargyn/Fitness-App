9d577c6727eff3d778a86e417cd470503bb8745d
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-03-05 10:31:37 -0600

    deleted unused files

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,24 +7,6 @@ import Ropes from "../images/ropes.jpeg";
 
 export const Home = () => (
   <div className="Whole-page">
-    {/* <div className="header">
-                <h1>EZBRO</h1>
-            </div>
-            <nav>
-
-            </nav>
-            <div className="content">
-                <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga vitae recusandae assumenda obcaecati architecto dolore eligendi rem ipsa possimus aliquam. Saepe, consequatur! Quam, quod quos velit fuga magnam exercitationem impedit?
-Facere repellat temporibus aliquam doloremque exercitationem dolores totam eveniet ex. Voluptatem, eos suscipit tempora mollitia itaque facere facilis quas expedita reiciendis eius ad provident dolorum enim quam, atque doloremque odit.
-Inventore placeat quia eveniet, sapiente cumque ad nam saepe ducimus impedit optio, asperiores deleniti reiciendis veniam error nesciunt. Maxime libero aspernatur aliquam maiores optio molestiae odio cum ab distinctio exercitationem?
-                </p>
-
-            </div>
-            <div className="footer">
-
-            </div> */}
-
     <Carousel>
       <Carousel.Item>
         <img className="d-block w-100" src={Weightbar} alt="First slide" />
