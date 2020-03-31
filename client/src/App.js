6910c36862a2eb1ebe11cb4f46225bb8f60bc863
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-03-31 14:11:20 -0500

    added README

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,58 @@
 import React, { useState } from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {Home} from "./pages/Home";
-import {Workouts} from "./pages/Workouts/index";
-import {Exercises} from "./pages/Exercises";
-import {Nutrition} from "./pages/Nutrition";
-import {Login} from "./pages/Login";
-import {NoMatch} from "./pages/NoMatch";
-import {PrivateRoute} from "./pages/PrivateRoutes";
-import {Layout} from "./components/layout";
-import {NavigationBar} from "./components/NavigationBar";
-import {Jumbotron} from "./components/Jumbotron";
-import {JumbotronBottom} from "./components/JumbotronBottom";
-import UserContext from "./store/store"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Workouts } from "./pages/Workouts/index";
+import { Exercises } from "./pages/Exercises";
+import { Nutrition } from "./pages/Nutrition";
+import { Login } from "./pages/Login";
+import { NoMatch } from "./pages/NoMatch";
+import { PrivateRoute } from "./pages/PrivateRoutes";
+import { Layout } from "./components/layout";
+import { NavigationBar } from "./components/NavigationBar";
+import { Jumbotron } from "./components/Jumbotron";
+import { JumbotronBottom } from "./components/JumbotronBottom";
+import UserContext from "./store/store";
 
 function App() {
+  const [user, setUser] = useState({
+    name: "Guest",
+    email: "",
+    age: "",
+    weight: "",
+    loggedIn: false
+  });
 
-
-    const [user, setUser] = useState({
-        name: "Guest",
-        email: "",
-        age: "",
-        weight: "",
-        loggedIn: false
-    });
-
-
-    return (
-            <React.Fragment>
-                <UserContext.Provider value={{user, setUser}}>
-                    <Router>
-                    <NavigationBar/>
-                    <Jumbotron/>
-                    <br></br>
-
-                    <Layout>
-
-                            <Switch>
-                                <Route exact path="/" component={Home}/>
-                                <PrivateRoute isLoggedIn={user.loggedIn} exact path="/workouts" component={Workouts}/>
-                                <Route exact path="/exercises" component={Exercises}/>
-                                <Route exact path="/nutrition" component={Nutrition}/>
-                                <Route exact path="/login" component={Login}/>
-                                <Route component={NoMatch}/>
-                            </Switch>
-
-                    </Layout>
-
-                    <br></br>
-                    <br></br>
-                    <div className="position-relative">
-                        <JumbotronBottom/>
-                    </div>
-                    </Router>
-                </UserContext.Provider>
-            </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Router>
+          <NavigationBar />
+          <Jumbotron />
+          <br></br>
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <PrivateRoute
+                isLoggedIn={user.loggedIn}
+                exact
+                path="/workouts"
+                component={Workouts}
+              />
+              <Route exact path="/exercises" component={Exercises} />
+              <Route exact path="/nutrition" component={Nutrition} />
+              <Route exact path="/login" component={Login} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
+          <br></br>
+          <br></br>
+          <div className="position-relative">
+            <JumbotronBottom />
+          </div>
+        </Router>
+      </UserContext.Provider>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- client/src/components/ExerciseList.js
+++ client/src/components/ExerciseList.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import { Container, Card, ListGroup, Row, Col, Image } from 'react-bootstrap';
-import axios from 'axios';
+import React from "react";
+import { Container, Card, ListGroup, Row, Col, Image } from "react-bootstrap";
+import axios from "axios";
 
- const ExerciseList = (props)=> {
-    return (
-        <div>
-            <Card>
-             <ListGroup varient="flush">
-              {props.exercises.map(el=><ListGroup.Item>
-                 <Container>
-                   <Row>
-                     <Col sm={12}>
-                       <Row>
-                         <h5>{el.name}</h5>
-                       </Row>
-                       <Row>
-                         <h6>{el.equipment}</h6>
-                       </Row>
-                       <Row>
-                         <p>{el.description}</p>
-                       </Row>
-                     </Col>
-                   </Row>
-                 </Container>
-               </ListGroup.Item>)
-              }
-             </ListGroup>
-           </Card>
-        </div>
-    )
+const ExerciseList = props => {
+  return (
+    <div>
+      <Card>
+        <ListGroup varient="flush">
+          {props.exercises.map(el => (
+            <ListGroup.Item>
+              <Container>
+                <Row>
+                  <Col sm={12}>
+                    <Row>
+                      <h5>{el.name}</h5>
+                    </Row>
+                    <Row>
+                      <h6>{el.equipment}</h6>
+                    </Row>
+                    <Row>
+                      <p>{el.description}</p>
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card>
+    </div>
+  );
+};
 
-}
-
-
-export default ExerciseList
+export default ExerciseList;
