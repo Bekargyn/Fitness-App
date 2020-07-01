02f6c2bdc15c8f1211b0e45830130bbc8932fa56
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-07-01 18:06:06 -0500

    removed unnecessary code

--- client/src/pages/Workouts/list.js
+++ client/src/pages/Workouts/list.js
@@ -1,63 +1,79 @@
-import React, {useEffect, useState} from "react";
-import {Container, Card, ListGroup, Row, Col, Image} from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Card, ListGroup, Row, Col } from "react-bootstrap";
 import axios from "axios";
 
 //List of workouts for user
 export const List = (props) => {
-    const [workoutList, setWorkoutList] = useState([]);
+  const [workoutList, setWorkoutList] = useState([]);
 
-    const getWorkoutList = () => {
-        axios.get("/api/workouts").then(data => {
-            setWorkoutList(Array.isArray(data.data) ? data.data : []);
-        });
-    };
+  const getWorkoutList = () => {
+    axios.get("/api/workouts").then((data) => {
+      setWorkoutList(Array.isArray(data.data) ? data.data : []);
+    });
+  };
 
-    useEffect(() => {getWorkoutList()}, []);
+  useEffect(() => {
+    getWorkoutList();
+  }, []);
 
+  const removeWorkout = (id) => {
+    if (!window.confirm("Are you sure you want to delete it?")) return 0;
+    axios({
+      method: "post",
+      url: "/api/workouts/delete",
+      data: {
+        id: id,
+      },
+    }).then(() => {
+      getWorkoutList();
+    });
+  };
 
-    const removeWorkout = (id) => {
-        if (!window.confirm("Are you sure you want to delete it?")) return 0;
-        axios({
-                    method: 'post',
-                    url: '/api/workouts/delete',
-                    data: {
-                        id: id
-                    }
-                }
-        ).then(()=>{getWorkoutList()});
-    }
-
-
-    return (
-            <div className="workout-list">
-                <div className="header">
-                    <h1>
-                        Workouts
-                        <button onClick={() => props.setCurrentPage("add-workouts")} className="btn btn-success float-right">
-                            Add
-                        </button>
-                    </h1>
-                </div>
-                <Card>
-                    <ListGroup varient="flush">
-                        {workoutList.map(el => (
-                                <ListGroup.Item key={el.id} onClick={() => {
-                                    props.setCurrentWorkout(el);
-                                    props.setCurrentPage("signle-workout")
-                                }}>
-                                    <Container>
-                                        <Row>
-                                            <Col sm={12} style={{"cursor":"pointer"}}>
-                                                <h5>{el.name}
-                                                    <button className="btn btn-danger float-right" onClick={(e) =>{ e.stopPropagation(); removeWorkout(el.id)}}>X</button>
-                                                </h5>
-                                            </Col>
-                                        </Row>
-                                    </Container>
-                                </ListGroup.Item>
-                        ))}
-                    </ListGroup>
-                </Card>
-            </div>
-    );
+  return (
+    <div className="workout-list">
+      <div className="header">
+        <h1>
+          Workouts
+          <button
+            onClick={() => props.setCurrentPage("add-workouts")}
+            className="btn btn-success float-right"
+          >
+            Add
+          </button>
+        </h1>
+      </div>
+      <Card>
+        <ListGroup varient="flush">
+          {workoutList.map((el) => (
+            <ListGroup.Item
+              key={el.id}
+              onClick={() => {
+                props.setCurrentWorkout(el);
+                props.setCurrentPage("signle-workout");
+              }}
+            >
+              <Container>
+                <Row>
+                  <Col sm={12} style={{ cursor: "pointer" }}>
+                    <h5>
+                      {el.name}
+                      <button
+                        className="btn btn-danger float-right"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          removeWorkout(el.id);
+                        }}
+                      >
+                        X
+                      </button>
+                    </h5>
+                  </Col>
+                </Row>
+              </Container>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card>
+    </div>
+  );
 };
